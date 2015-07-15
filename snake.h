1bdee2469ce87c915ceb38d6359029039533fc1c
Thomas Vakili <touzen74@gmail.com>
2015-07-15 12:29:15 +0200

    Create snake.h
    This header file contains the data structures and functions that are used to manage the user controlled snake. The actual snake is implemented as a linked list of snake "segments".

---
+++ snake.h
@@ -0,0 +1,52 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+/**
+ * Data structure to represent a snake. Implemented as en enhanced linked list of Segment nodes.
+*/
+typedef struct {
+    Segment* head;
+    Segment* end;
+    int length;
+} Snake;
+
+/**
+ * Data structure to represent a snake segment. Basically a node in a linked list.
+*/
+typedef struct {
+    Direction* direction
+    int x_pos;
+    int y_pos;
+    Segment* next;
+} Segment;
+
+/**
+ * Create a new snake at the specified coordinates.
+*/
+Snake* new_snake(int x, int y);
+
+/**
+ * Delete a snake and free the memory associated with it.
+*/
+void delete_snake(Snake* snake);
+
+/**
+ * Adds a segment to the snake.
+*/
+void add_segment(Snake* snake);
+
+/**
+ * Update direction of the snake based on the move stack.
+*/
+void update_direction(Snake* snake, MoveList* moves);
+
+/**
+ * Moves each segment of the snake one step.
+*/
+void move(Snake* snake);
+
+/**
+ * Given a snake, determine whether or not it has collided with itself.
+*/
+int find_collision(Snake snake);
+#endif
