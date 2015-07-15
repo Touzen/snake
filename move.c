6931092065f014e073eae5d12b93d6b5b34f065a
Thomas Vakili <touzen74@gmail.com>
2015-07-15 12:24:42 +0200

    Created and implemented move.c
    This code specifies operations that work with the MoveList structure, which will be specified in the header file "move.h".

---
+++ move.c
@@ -0,0 +1,40 @@
+#include "move.h"
+
+MoveList* new_move_list() {
+    MoveList* ms = malloc(sizeof(MoveList));
+    *ms = (MoveList) {.top = NULL, length = 0};
+    return ms;
+}
+
+void delete_move_list(MoveList* list) {
+    while(list->oldest != NULL) {
+        pop_move(list);
+    }
+
+    free(snake);
+}
+
+void pop_move(MoveList* list) {
+    if (list->oldest == NULL) {
+        return;
+    }
+
+    MoveItem* old_oldest = list->oldest;
+    list->oldest = list->oldest->next;
+
+    if (list->oldest != NULL) {
+        list->oldest->prev = NULL;
+    }
+
+    free(old_oldest);
+}
+
+void push_move(MoveList* list, Direction direction) {
+    MoveItem* move = malloc(sizeof(MoveItem));
+    
+    move->direction = direction;
+    move->age = 0;
+    
+    move->prev = list->latest;
+    list->latest = move;
+}
