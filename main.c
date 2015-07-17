041a5de29c40a87c06d5cf1c21917e83a14dbd76
Thomas Vakili <thomas@vakili.net>
2015-07-17 19:10:32 +0200

    Fixed broken pointer handling that lead to crashing.
    When the snake escaped, the code tried to free the memory of the global variables in the main code. This error was made because of confusion regarding the fact that while the structs were created on the heap, dereferncing the result of the "constructor" only copied this value to the global variables. Thus, freeing them made no sense. Also, this fixes a minor memory leak. ;)

--- main.c
+++ main.c
@@ -6,44 +6,44 @@
 
 #define SLEEP_TIME 1
 
-MoveList move_list;
-Snake snake;
-Screen screen;
+MoveList* move_list;
+Snake* snake;
+Screen* screen;
 
 void init() {
-    screen = *(new_screen());
+    screen = new_screen();
 
-    int x_pos = screen.max_x/2;
-    int y_pos = screen.max_y/2;
+    int x_pos = screen->max_x/2;
+    int y_pos = screen->max_y/2;
 
-    snake = *(new_snake(x_pos, y_pos));
-    move_list = *(new_move_list());
+    snake = new_snake(x_pos, y_pos);
+    move_list = new_move_list();
 }
 
 void end_game() {
-    close_screen(&screen);
-    delete_snake(&snake);
-    delete_move_list(&move_list);
+    close_screen(screen);
+    delete_snake(snake);
+    delete_move_list(move_list);
 }
 
 int tick() {
     // Handle user input
     Direction in = get_direction();
     if (in != NO_DIRECTION) {
-        push_move(&move_list, in);
+        push_move(move_list, in);
     }
     
     // Handle snake movement
-    update_direction(&snake, &move_list);
-    move_snake(&snake);
+    update_direction(snake, move_list);
+    move_snake(snake);
 
     // Draw everything
-    int draw_success = draw_frame(&snake, &screen);
+    int draw_success = draw_frame(snake, screen);
     if (!draw_success) {
         return 0;   // Snake has tried to escape! ;)
     }
     
-    if (find_collision(&snake)) {
+    if (find_collision(snake)) {
         return 0;
     }
     
